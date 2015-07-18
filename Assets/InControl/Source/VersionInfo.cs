cc786aba7e8133962c398c47305df67db1d91356
BuildTools <unconfigured@null.spigotmc.org>
2015-07-17 23:22:10 -0700

    Fixed?

--- Assets/InControl/Source/VersionInfo.cs
+++ Assets/InControl/Source/VersionInfo.cs
@@ -28,11 +28,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 4,
-<<<<<<< HEAD
-				Build = 3831
-=======
-				Build = 3813
->>>>>>> origin/master
+				Build = 3835
 			};
 		}
 

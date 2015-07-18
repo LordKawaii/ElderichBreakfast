da54ac17664b98649648bdf30034ec6fbbcf32d5
Jase Pellerin <jasepellerin@gmail.com>
2015-07-17 23:17:52 -0700

    Eh

--- Assets/InControl/Source/VersionInfo.cs
+++ Assets/InControl/Source/VersionInfo.cs
@@ -28,7 +28,15 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 4,
+<<<<<<< HEAD
 				Build = 3832
+=======
+<<<<<<< HEAD
+				Build = 3831
+=======
+				Build = 3813
+>>>>>>> origin/master
+>>>>>>> origin/master
 			};
 		}
 

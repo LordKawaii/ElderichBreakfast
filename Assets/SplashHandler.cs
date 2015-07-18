e4d1b9a30c8bf6a0ab93039e4dec7262793823e7
Jase Pellerin <jasepellerin@gmail.com>
2015-07-18 13:39:11 -0700

    Splash Complete

--- Assets/SplashHandler.cs
+++ Assets/SplashHandler.cs
@@ -5,13 +5,15 @@ public class SplashHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Invoke("nextScene", 5);
+		Invoke("startFade", 5);
 	}
 	
-	void nextScene () { 
+	void startFade () { 
 		float fadeTime = GameObject.Find ("FadeManager").GetComponent<Fading> ().BeginFade (1);
-		Debug.Log (fadeTime);
-		yield WaitForSeconds (1);
+		Invoke ("nextScreen", fadeTime);
+	}
+
+	void nextScreen () {
 		Application.LoadLevel (Application.loadedLevel + 1);
 	}
 }

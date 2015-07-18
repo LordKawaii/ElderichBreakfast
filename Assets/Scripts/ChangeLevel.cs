80aae2f8a462f5a91aae28db11a3396eb5d26190
Jase Pellerin <jasepellerin@gmail.com>
2015-07-18 13:35:00 -0700

    Updates

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	IEnumerator FadeOut () {
+		float fadeTime = GameObject.Find ("FadeManager").GetComponent<Fading> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
---
+++ Assets/SplashHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("nextScene", 5);
+	}
+	
+	void nextScene () { 
+		float fadeTime = GameObject.Find ("FadeManager").GetComponent<Fading> ().BeginFade (1);
+		Debug.Log (fadeTime);
+		yield WaitForSeconds (1);
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
